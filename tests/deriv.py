e3e1b88f08c9b7bd0eea36612f7406d8c0250e5b
zazbone <coczaz@gmail.com>
2021-06-06 17:52:18 +0200

    Test to show that derivation curently suck

--- tests/deriv.py
+++ tests/deriv.py
@@ -1,4 +1,4 @@
-from PDE import System, animations, MODULE_PATH
+from PDE import Derivator, animations, MODULE_PATH
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,26 +6,32 @@ import matplotlib.pyplot as plt
 stop = 10
 step = 100
 
-deriv = System.create_derive(order=4, derive_order=1, h=stop / step)
 t = np.linspace(0, stop, step)
+deriv = Derivator(t.shape, 1, 4, 0.1, lambda x: x)
 
 plt.plot(t, np.cos(t), 'r')
-plt.plot(t, deriv(np.sin(t)), 'b')
+plt.plot(t, deriv._deriv[1][0](np.sin(t)), 'b')
 plt.show()
 
 
 delat_t = 0.1
-delta_x = 0.1
+delta_x = 0.2
 x_max = 10
 t_max = 10
 c = 0.3
-derive2 = System.create_derive(4, 1, delta_x)
+
+def b(u):
+    u[0] = 2
+    u[-1] = 2
+    return u
+
 x = np.arange(0, x_max, delta_x)
 u = np.sin(x) + 2
+derive2 = Derivator(x.shape, 2, 4, delta_x, b)
 image_array = [u]
 for _ in np.arange(0, t_max, delat_t):
-    print(derive2(u)[0])
-    u = u + delat_t * c * derive2(u)
+    u = u + delat_t * c * derive2.D(1,0,u)
     image_array.append(u)
 
-animations.anim1D(x, image_array, delat_t, MODULE_PATH.parent / "tests/test1.mp4")
+animations.anim1D(x, image_array, delat_t, MODULE_PATH.parent / "tests/out.mp4")
+
